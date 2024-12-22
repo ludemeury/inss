a0526c2f030a59ed40400310a1b73a4b26194aed
Ludemeury Fernandes de Sa <ludemeury@gmail.com>
2024-12-22 18:46:27 -0300

    chore: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Proponent.find_or_create_by!(name: 'João Batista de Alburgueque', birthdate: 30.years.ago,
+income: 2050)
+Proponent.find_or_create_by!(name: 'Alino dos Santos Ribeiro',
+birthdate: 18.years.ago, income: 3045.54)
+Proponent.find_or_create_by!(name: 'Carlos dos Santos',
+birthdate: 25.years.ago, income: 4355.65)
+Proponent.find_or_create_by!(name: 'Roberto Campos', birthdate: 33.years.ago,
+income: 18345.32)
+Proponent.find_or_create_by!(name: 'Andre Santos', birthdate: 19.years.ago,
+income: 3321.32)
+Proponent.find_or_create_by!(name: 'Valquiria Martins',
+birthdate: 19.years.ago, income: 4500)
+Proponent.find_or_create_by!(name: 'Ronivaldo Madeiros',
+birthdate: 25.years.ago, income: 9567)
+Proponent.find_or_create_by!(name: 'Sandro Pereira', birthdate: 32.years.ago,
+income: 7879)
+Proponent.find_or_create_by!(name: 'José Mendes', birthdate: 19.years.ago,
+income: 1790)
+Proponent.find_or_create_by!(name: 'Janaína Lopes', birthdate: 22.years.ago,
+income: 1234)
+Proponent.find_or_create_by!(name: 'Jesse Coimbra', birthdate: 20.years.ago,
+income: 1411)
