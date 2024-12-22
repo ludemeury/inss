01e053cbd2d7a04ab636cbea8b301c271041d2dd
Ludemeury Fernandes de Sa <ludemeury@gmail.com>
2024-12-22 18:52:08 -0300

    chore: add rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 Proponent.find_or_create_by!(name: 'João Batista de Alburgueque', birthdate: 30.years.ago,
-income: 2050)
+                             income: 2050)
 Proponent.find_or_create_by!(name: 'Alino dos Santos Ribeiro',
-birthdate: 18.years.ago, income: 3045.54)
+                             birthdate: 18.years.ago, income: 3045.54)
 Proponent.find_or_create_by!(name: 'Carlos dos Santos',
-birthdate: 25.years.ago, income: 4355.65)
+                             birthdate: 25.years.ago, income: 4355.65)
 Proponent.find_or_create_by!(name: 'Roberto Campos', birthdate: 33.years.ago,
-income: 18345.32)
+                             income: 18_345.32)
 Proponent.find_or_create_by!(name: 'Andre Santos', birthdate: 19.years.ago,
-income: 3321.32)
+                             income: 3321.32)
 Proponent.find_or_create_by!(name: 'Valquiria Martins',
-birthdate: 19.years.ago, income: 4500)
+                             birthdate: 19.years.ago, income: 4500)
 Proponent.find_or_create_by!(name: 'Ronivaldo Madeiros',
-birthdate: 25.years.ago, income: 9567)
+                             birthdate: 25.years.ago, income: 9567)
 Proponent.find_or_create_by!(name: 'Sandro Pereira', birthdate: 32.years.ago,
-income: 7879)
+                             income: 7879)
 Proponent.find_or_create_by!(name: 'José Mendes', birthdate: 19.years.ago,
-income: 1790)
+                             income: 1790)
 Proponent.find_or_create_by!(name: 'Janaína Lopes', birthdate: 22.years.ago,
-income: 1234)
+                             income: 1234)
 Proponent.find_or_create_by!(name: 'Jesse Coimbra', birthdate: 20.years.ago,
-income: 1411)
+                             income: 1411)
