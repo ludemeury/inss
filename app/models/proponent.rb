3e739918056320e4bfec0149c22a3c727a998018
Ludemeury Fernandes de Sa <ludemeury@gmail.com>
2024-12-23 22:50:27 -0300

    feat: layout and validates proponents

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class Proponent < ApplicationRecord
+  validates :name, presence: true
+
+  validates :birthdate, presence: true
+
+  validates :income, numericality: { greater_than: 0 }
+
   has_many :addresses, as: :addressable, dependent: :destroy
 end
