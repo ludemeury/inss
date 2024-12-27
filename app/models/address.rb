090cf547d5d4b498b7e4e0f6ef73281ed952bbd3
Ludemeury Fernandes de Sa <ludemeury@gmail.com>
2024-12-27 11:57:17 -0300

    feat: validation address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+
 class Address < ApplicationRecord
+  KINDS = %w[residencial comercial].freeze
+
   belongs_to :addressable, polymorphic: true
+
+  validates :kind, inclusion: { in: KINDS }
+
+  validates :street, presence: true
+
+  validates :city, presence: true
+
+  validates :state, presence: true
+
+  validates :zip_code, numericality: { only_integer: true }
+
+  validates :number, presence: true
+
+  validates :district, presence: true
 end
