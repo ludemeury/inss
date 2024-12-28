488e95f9a901cc8dabe2d0e1f984ad2eba175a9b
Ludemeury Fernandes de Sa <ludemeury@gmail.com>
2024-12-27 21:08:06 -0300

    feat: add sidekiq

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Run database setup if needed
-if [ "$RAILS_ENV" != "production" ]; then
-  bundle exec rails assets:precompile
-  echo "Running migrations..."
-  bundle exec rails db:prepare
-fi
+# if [ "$RAILS_ENV" != "production" ]; then
+#   bundle exec rails assets:precompile
+#   echo "Running migrations..."
+#   bundle exec rails db:prepare
+# fi
 
 # Execute the passed commands
 exec "$@"
