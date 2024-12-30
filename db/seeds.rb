07d01d8427fa8614ca24a904f1889affbea222a4
Ludemeury Fernandes de Sa <ludemeury@gmail.com>
2024-12-30 01:00:07 -0300

    chore: consolidade dash on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ admin_user = "admin@admin.com"
 User.create!(email_address: admin_user, password: "123456", password_confirmation: "123456") unless User.where(email_address: admin_user).any?
 
 FactoryBot.create_list(:proponent, 15) unless Proponent.any?
+
+ConsolidateProponentReportJob.new.perform
